86c7687dc415be96acdf9d10afab2f504a370aa8
muembiahub <jonathanmuembia3@gmail.com>
2026-03-08 20:05:48 +0200

    cool

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,10 +1,6 @@
 import express from "express";
 import serverless from "serverless-http";
 import dotenv from "dotenv";
-import helmet from "helmet";
-import path from "path";
-import { fileURLToPath } from "url";
-import slugify from "slugify";
 import categoriesRouter from "../../src/routes/categories.js";
 import servicesRouter from "../../src/routes/services.js";
 import contactRoutes from "../../src/routes/contact.js";
@@ -15,7 +11,6 @@ dotenv.config();
 const app = express();
 const router = express.Router();
 
-// ⚡️ Monte tes routes Express
 router.use("/categories", categoriesRouter);
 router.use("/services", servicesRouter);
 router.use("/", contactRoutes);
@@ -24,7 +19,6 @@ router.get("/", (req, res) => {
   res.send("Accueil Romolayte via Netlify Functions");
 });
 
-// ⚡️ Important : toutes les routes passent par /.netlify/functions/server
 app.use("/.netlify/functions/server", router);
 
 export const handler = serverless(app);
