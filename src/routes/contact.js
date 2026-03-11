00570c99bfa38eb63ccc0130afa750e5c03655be
muembiahub <jonathanmuembia3@gmail.com>
2026-03-11 12:26:28 +0200

    well

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -1,5 +1,6 @@
 import express from "express";
-const { createClient } = require("@supabase/supabase-js");
+import { createClient } from "@supabase/supabase-js";
+
 
 
 const router = express.Router();
